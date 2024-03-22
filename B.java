07ead3cb9c184343c67127237aac4f4483be1641
Eneko <149537780+enekocm7@users.noreply.github.com>
2024-03-22 17:13:15 +0100

    Creados los métodos Getter en B

--- B.java
+++ B.java
@@ -1,5 +1,12 @@
 public class B {
     private String nombre;
     private String apellido;
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
     
 }
