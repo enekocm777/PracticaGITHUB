b5f4e143d60bdb513a67bc75569ec6c05432fc39
Eneko <149537780+enekocm7@users.noreply.github.com>
2024-03-22 17:15:50 +0100

    Creado constructor en B

--- B.java
+++ B.java
@@ -2,6 +2,11 @@ public class B {
     private String nombre;
     private String apellido;
 
+    public B(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
     public String getNombre(){
         return this.nombre;
     }
@@ -12,8 +17,10 @@ public class B {
         this.nombre=nombre;
     }
     public void setApellido(String apellido){
-        this.napellido=apellido;
+        this.apellido=apellido;
     }
+
+    
         
     
 }
